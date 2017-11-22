e1e4111acc06afece71cf6f584ef6b7021d65221
Derek Schaefer <derek.schaefer@gmail.com>
2017-11-22 14:51:21 -0600

    init

---
+++ lib/ddwrt.ex
@@ -0,0 +1,18 @@
+defmodule Ddwrt do
+  @moduledoc """
+  Documentation for Ddwrt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ddwrt.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ddwrt_test.exs
@@ -0,0 +1,8 @@
+defmodule DdwrtTest do
+  use ExUnit.Case
+  doctest Ddwrt
+
+  test "greets the world" do
+    assert Ddwrt.hello() == :world
+  end
+end
